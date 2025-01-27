41aad3c57bf2515582ab97e441d6fc61f4fa7154
Maxim Khrypunov <khrypunovmax@gmail.com>
2025-01-27 17:42:02 +0200

    solution1

--- musician/models.py
+++ musician/models.py
@@ -1,5 +1,21 @@
+from audioop import minmax
+
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
 class Musician(models.Model):
-    pass
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+    instrument = models.CharField(max_length=63)
+    age = models.IntegerField(validators=[MinValueValidator(14),
+                                          MaxValueValidator(99)])
+    date_of_applying = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def is_adult(self):
+        return self.age >= 21
